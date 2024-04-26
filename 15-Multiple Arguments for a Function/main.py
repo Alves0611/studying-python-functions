37ab646ebc1b222d6bc26de132c6b9b2e99b977d
Alves0611 <g.alves0611@gmail.com>
2024-04-26 13:28:33 -0300

    done

--- 15-Multiple Arguments for a Function/main.py
+++ 15-Multiple Arguments for a Function/main.py
@@ -6,3 +6,17 @@ def my_sum(*numbers):
     return total
 
 print(my_sum(15, 17, 13, 16))
+
+
+
+def final_price(price, **extras):
+    print(extras)
+    if 'discount' in extras:
+        price *= (1 - extras['discount'])
+    if 'extended_warranty' in extras:
+        price += extras['extended_warranty']
+    if 'tax' in extras:
+        price *= (1 + extras['tax'])
+    return price
+
+print(final_price(1000, discount=0.1, extended_warranty=100, tax=0.3))
